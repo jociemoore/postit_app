0b7a5cc0d219b9de035a558884cca101a293b8fb
Jocie Moore <hello@jociemoore.com>
2018-08-30 09:54:11 -0700

    Add user authentication

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   has_many :comments
-  has_many :post_categories, dependent: :destroy
+  has_many :post_categories
   has_many :categories, through: :post_categories
 
   validates :title, presence: true, length: {minimum: 5}
