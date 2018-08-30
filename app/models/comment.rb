8a4ef7b0786a118e50053f409cd064bf90269cbd
Jocie Moore <hello@jociemoore.com>
2018-08-30 11:48:19 -0700

    Setup model and database table for Vote

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   belongs_to :post
+  has_many :votes, as: :voteable, dependent: :destroy
 
   validates :body, presence: true
 end
